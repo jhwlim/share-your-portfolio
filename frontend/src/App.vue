0f952194787eb7d400cd71cb005a58b3262bdda1
jh Lim <jhwlim25@gmail.com>
2021-06-07 23:14:03 +0900

    axios 추가 및 api 호출 테스트 예제 작성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,28 @@
   <div id="app">
     face
     <span class="material-icons md-18">face</span>
+    <button @click="testApi()">API 테스트</button>
   </div>
 </template>
 
 <script>
 import '@/assets/css/static/reset.css';
 import '@/assets/css/static/app.css'
+import TestApi from '@/api/TestApi.js'
 
 export default {
   name: 'App',
+  methods: {
+    testApi() {
+      TestApi.test()
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      TestApi.test1();
+    },
+  },
 }
 </script>
