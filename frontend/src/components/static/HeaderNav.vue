c6126889c7f69fbbbfc3e8c1075390056e6a9756
jh Lim <jhwlim25@gmail.com>
2021-06-08 19:03:07 +0900

    HeaderNav 디자인

--- frontend/src/components/static/HeaderNav.vue
+++ frontend/src/components/static/HeaderNav.vue
@@ -1,18 +1,97 @@
 <template>
-    <div>
-        header
+    <div class="header-nav">
+        <span class="header-nav__name">LOGO</span>
+        <div class="header-nav__links">
+            <div v-if="isLogined"  class="header-nav__link">
+                <span @click="changeDropboxState()" class="material-icons header-nav__icon pointer" :class="clickedIconStyle">account_circle</span>
+                <ul v-if="showDropbox" class="header-nav__drop-box shadow">
+                    <li class="pointer">계정 관리</li>
+                    <li class="pointer">채팅</li>
+                    <li class="pointer">로그아웃</li>
+                </ul>
+            </div>
+            <div v-else class="header-nav__link">
+                <span class="header-nav__icon-text pointer">로그인</span>
+                <span class="header-nav__icon-text pointer">회원가입</span>
+            </div>
+        </div>        
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'HeaderNav',
+    data: function() {
+        return  {
+            isLogined: false,
+            showDropbox: false,
+        };
+    },
+    computed: {
+        clickedIconStyle() {
+            return this.showDropbox ? 'header-nav__icon--clicked' : null;
+        },
+    },
+    methods: {
+        changeDropboxState() {
+            this.showDropbox = !this.showDropbox;
+        },
+    }
 }
 </script>
 
 <style scoped>
-div {
-    font-size: 40px;
-    font-weight: 700;
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap');
+
+.header-nav {
+    padding: 18px 40px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #f5f5f5;
+}
+.header-nav__name {
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 46px;
+}
+.header-nav__link {
+    display: flex;
+    align-items: center;
+    position: relative;
+}
+.header-nav__icon-text:hover {
+    color: gray;
+}
+.header-nav__icon {
+    font-size: 42px;
+}
+.header-nav__icon--clicked {
+    color: #f5f5f5;
+    background-color: black;
+    border-radius: 50%;
+}
+.header-nav__icon-text {
+    font-size: 20px;
+    font-weight: 300;
+    letter-spacing: 1px;
+    margin-left: 8px;
+}
+.header-nav__drop-box {
+    position: absolute;
+    top: 46px;
+    right: 0;
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 6px 0;
+    width: 140px;
+    text-align: center;
+    background-color: white;
+}
+.header-nav__drop-box li {
+    padding: 10px 0;
+    border-bottom: 1px solid #dadada;
+}
+.header-nav__drop-box li:last-child {
+    border-bottom: 0;
 }
 </style>
