a4186b3ba01e2faad37c8059e220e2ae29c3972e
jh Lim <jhwlim25@gmail.com>
2021-06-13 14:39:09 +0900

    LoginView 유효성 검사 추가

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,13 +5,16 @@
             <form class="login-form">
                 <div class="login-form__input-box">
                     <label for="username">아이디</label>
-                    <input type="text" id="username" autocomplete="off">
+                    <input type="text" id="username" v-model="username" autocomplete="off">
+                    <span class="error-message">{{ errorMessage.username }}</span>
                 </div>
                 <div class="login-form__input-box">
                     <label for="password">패스워드</label>
-                    <input type="password" id="password" autocomplete="new-password">
+                    <input type="password" id="password" v-model="password" autocomplete="new-password">
+                    <span class="error-message">{{ errorMessage.password }}</span>
                 </div>
-                <div class="login-form__btn pointer">로그인</div>
+                <span class="error-message">{{ errorMessage.login }}</span>
+                <div class="login-form__btn pointer" @click="submitLoginForm()">로그인</div>
             </form>
             <div class="social-login">
                 <div class="social-login__icon pointer">
@@ -32,8 +35,51 @@
 export default {
     data: function() {
         return {
-            
+            username: '',
+            password: '',
+            errorMessage: {
+                username: '',
+                password: '',
+                login: '',
+            },
         };
+    },
+    methods: {
+        submitLoginForm() {
+            this.clearErrorMessage();
+
+            const isValidUsername = this.checkUsername();
+            const isValidPassword = this.checkPassword();
+
+            if (isValidUsername && isValidPassword) {
+                console.log('로그인 시도');
+                
+                // 로그인 성공 -> 페이지 변경, 헤더 변경하기, vuex 값 저장
+
+                // 로그인 실패 -> 에러메시지 출력, 아이디/비밀번호 초기화
+                this.errorMessage.login = '가입하지 않은 아이디이거나, 잘못된 비밀번호 입니다.';
+            }
+
+        },
+        checkUsername() {
+            if (this.username.length == 0) {
+                this.errorMessage.username = '아이디를 입력해주세요';
+                return false;
+            }
+            return true;
+        },
+        checkPassword() {
+            if (this.password.length == 0) {
+                this.errorMessage.password = '비밀번호를 입력해주세요';
+                return false;
+            }
+            return true;
+        },
+        clearErrorMessage() {
+            this.errorMessage.username = '';
+            this.errorMessage.password = '';
+            this.errorMessage.login = '';
+        }
     },
 }
 </script>
@@ -56,13 +102,14 @@ export default {
     text-align: center;
 }
 .login-form__input-box {
-    margin-bottom: 20px;
+    margin-bottom: 16px;
 }
 .login-form__input-box label {
     display: block;
     margin-bottom: 6px;
 }
 .login-form__input-box input {
+    display: block;
     width: 260px;
     padding: 6px;
     border: 0;
@@ -78,6 +125,7 @@ export default {
     padding: 10px 0;
     text-align: center;
     background-color: lightgray;
+    margin-top: 6px;
 }
 .social-login {
     padding: 20px;
@@ -96,5 +144,9 @@ export default {
 }
 .social-login__icon--small img {
     width: 80%;
+}
+.error-message {
+    color: red;
+    font-size: 12px;
 }
 </style>
