1679f0fe50ed2c0dbb0acd78990dba7d7cbfe4da
jh Lim <jhwlim25@gmail.com>
2021-06-17 11:45:08 +0900

    PostView 페이지 접근 시 id 값 설정하기

--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -6,7 +6,23 @@
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            id: null,
+        };
+    },
+    watch: {
+        '$route': 'fetchData',
+    },
+    methods: {
+        fetchData() {
+            this.id = this.$route.params.id;
+            console.log(this.id);
+        },
+    },
+    created: function() {
+        this.fetchData();
+    },
 }
 </script>
 
